056c28536d30cbd12cd1d70a2e93797ebf211935
austinwu123 <austinwu819@gmail.com>
2026-06-12 17:55:58 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,8 @@
+count = int(input("How many times do you want to add? "))
+step = float(input("What do you want to count by? "))
 
+total = 0
+
+for i in range(count):
+    total = total + step
+    print(f"Round {i + 1}: {total}")
